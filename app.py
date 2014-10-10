7c59718cdc56a6e69320ce963f483ca20e89b654
Chesley Tan <chesleytan97@gmail.com>
2014-10-10 17:19:30 -0400

    Finish search page; Add template for answer page

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html");
+    return render_template("index.html")
 
+@app.route("/answer", methods=["GET"])
+def answer():
+    query = request.args.get('q')
+    return render_template("answer.html")
 
 if __name__ == "__main__":
     app.debug = True
