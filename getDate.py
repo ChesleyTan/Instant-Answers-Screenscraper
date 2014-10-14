fd5d6448debef915a889bb4be8101e98877c9178
David Bang <davidbang2012@gmail.com>
2014-10-13 23:28:00 -0400

    Edited getDate.py

--- getDate.py
+++ getDate.py
@@ -1,14 +1,41 @@
-import re, scraper
+import re
+from collections import Counter
+import time
 
-r = scraper.scrape()
+def filterDateandTime (r):
+    results = Counter()
+#source http://stackoverflow.com/questions/10086980/date-matching-using-python-regex
+    regex_dates = "\d{1,2}[-/:]\d{1,2}[-/:]\d{2,4}"
+    regex = re.compile(regex_dates)
+    datestimes = regex.findall (r)
+    for a in findDate(r):
+        datestimes.append(a)
+    #print datestimes
+    for b in findTime(r):
+        datestimes.append(b)
+    for c in datestimes:
+        results [c] += 1
+    return results
+    
+
 def findDate(r):
-    listOfDates = re.findall("Jan|January|Feb|February|Mar|March|Apr|April|May|Jun|June|Jul|July|Aug|August|Sep|September|Oct|October|Nov|November|Dec|December", r)
-    return listOfDates
+    listOfDates = re.findall("(?:(January|February|March|April|May|June|July|August|September|October|November|December) +(0?[1-9]|[12]\d|3[01])?, *([0-9]*)?)", r)
+    listdates =[] 
 
+    for l in listOfDates:
+        listdates.append( l [0] + " " + l[1] + ", " + l[2])
+        
+    return listdates
 
 def findTime(r):
-    listOfTimes = re.findall("[0-9]{2}:[0-9]{2}", r)
+    listOfTimes = re.findall("\d{1,2}[:]\d{1,2}", r)
     return listOfTimes
 
-print "Times: " + str(findTime(r))
-print "Dates: " + str(findDate(r))
+
+if __name__ == "__main__":
+    r = findDate("January 29, 2014, January 15, 2013")
+    print (r)
+    r = findTime("9:30")
+    print (r)
+    r = filterDateandTime("10:30 11-11-2012 11/12/2014 January 29, 2014, January 15, 2013")
+    print(r)
