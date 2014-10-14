952616d1af050cd44b0d67690294dd21927003f5
David Bang <davidbang2012@gmail.com>
2014-10-13 23:41:39 -0400

    added when to app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import filterNames
 from collections import Counter
 from socket import timeout
 from ssl import SSLError
+import getDate
 
 app = Flask(__name__)
 
@@ -59,6 +60,46 @@ def answer():
         retStr = "<h1>The most common name was " + topNames.most_common(1)[0][0] + "</h1>" + retStr
 
         return retStr
+
+    elif query.upper().find("WHEN") > -1:
+        try:
+            urls = [x for x in google.search(query, lang='en', num=10, start=0, stop=9, pause=1.0)]
+        except urllib2.URLError:
+            pass
+        print "Got urls :)"
+        pages = []
+        retStr = ""
+        topdt = Counter()
+        for url in urls:
+            try:
+                soup = BeautifulSoup(urlopen(url, timeout=1).read())
+            except urllib2.URLError:
+                continue
+            except timeout:
+                continue
+            except SSLError:
+                continue
+            print "Got soup :)"
+            pages.append(soup.get_text())
+            for page in pages:
+                dts = getDate.filterDateandTime(page).most_common(10)
+                if len(dts) > 0:
+                    dt = dts[0][0]
+                    if not(' ' in dt):
+                        for item in topdt:
+                            parts = item.split(" ")
+                            if len(parts) > 1:
+                                if dt in parts:
+                                    topdt[item] += 1
+                                    print "Increasing confidence of " + item + " with " + dt
+                    topdt[dt] += 1
+            retStr += "<table>"
+            for _tuple in dts:
+                retStr += "<tr><td>" + _tuple[0] + "</td><td>" + str(_tuple[1]) + "</td></tr>"
+            retStr += "</table><hr>"
+        retStr = "<h1>The most common date or time  was " + topdt.most_common(1)[0][0] + "</h1>" + retStr
+        return retStr
+
     elif query_upper.find("WHERE") > -1:
         if "AM" in query_upper or "am" in query_upper:
         # Self referential case
@@ -112,47 +153,47 @@ def answer():
             return retStr
 
 
-    elif query_upper.find("WHEN") > -1:
-        try:
-            urls = [x for x in google.search(query, lang='en', num=10, start=0, stop=9, pause=1.0)]
-        except urllib2.URLError:
-            pass
-        print "Viable URLs found"
-        retStr = ""
-        pages = []
-        stored = []
-        try:
-            true_query = query_upper.split(' ')[2:]
-        except:
-            true_query = query_upper.split(' ')[-2:]
-        for url in urls[:10]:
-            try:
-                soup = BeautifulSoup(urlopen(url, timeout=1).read())
-            except urllib2.URLError:
-                continue
-            except timeout:
-                continue
-            except SSLError:
-                continue
-            print "Soup's up"
-        try:
-            pages.append(soup.get_text())
-        except:
-            return "Nothing found :("
-        for page in pages:
-            sentences = page.split('.')
-            potential_keywords = ['time', 'date', 'schedule', 'when', 'year', 'month', 'day', 'year']
-            for sentence in sentences:
-                for keyword in potential_keywords:
-                    if keyword in sentence or keyword.title() in sentence:
+#    elif query_upper.find("WHEN") > -1:
+#        try:
+#            urls = [x for x in google.search(query, lang='en', num=10, start=0, stop=9, pause=1.0)]
+#        except urllib2.URLError:
+#            pass
+#        print "Viable URLs found"
+#        retStr = ""
+#        pages = []
+#        stored = []
+#        try:
+#            true_query = query_upper.split(' ')[2:]
+#        except:
+#            true_query = query_upper.split(' ')[-2:]
+#        for url in urls[:10]:
+#            try:
+#                soup = BeautifulSoup(urlopen(url, timeout=1).read())
+#            except urllib2.URLError:
+#                continue
+#            except timeout:
+#                continue
+#            except SSLError:
+#                continue
+#            print "Soup's up"
+#        try:
+#            pages.append(soup.get_text())
+#        except:
+#            return "Nothing found :("
+#        for page in pages:
+#            sentences = page.split('.')
+#            potential_keywords = ['time', 'date', 'schedule', 'when', 'year', 'month', 'day', 'year']
+#            for sentence in sentences:
+#                for keyword in potential_keywords:
+#                    if keyword in sentence or keyword.title() in sentence:
                         # Since it was sorted by prioritized pages and keywords,
                         # We'll use that as the rank (first found = naturally better)
-                        stored += [sentence]
-        try:
-            retStr = "The most likely answer is: " +stored[0]
-        except:
-            retStr = "Nothing found :("
-        return retStr
+#                        stored += [sentence]
+#        try:
+#            retStr = "The most likely answer is: " +stored[0]
+#        except:
+#            retStr = "Nothing found :("
+#        return retStr
     else:
         return "Query not supported"
     #return render_template("answer.html")
