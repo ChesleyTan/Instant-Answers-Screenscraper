e635d8ee37331da175732ad17e2d80c7ae90602a
eshoyfer <eshoyfer@gmail.com>
2014-10-13 22:48:40 -0400

    Misc. Beautiful Soup tests

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 #pip install beautifulsoup4 and requests on your virtual env
 #http://stackoverflow.com/questions/24458353/cleaning-text-string-after-getting-body-text-using-beautifulsoup
@@ -8,6 +9,8 @@ def bsoup():
     r = requests.get("https://answers.yahoo.com/question/index?qid=20080613085817AAqvcNW")
     soup = BeautifulSoup (open(r.content))
     string = soup.findall("div", {"class":"group"})
+    print soup.finalall(re.compile("^[A-Z]"))
+    print soup.prettify()
     print string
 
 
